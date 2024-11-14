fd982ec42d1e17bed4a306e86d277fec6da50879
UsmanAamir01 <f223709@cfd.nu.edu.pk>
2024-11-14 22:39:34 +0500

    Feature A initial changes

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -1,6 +1,8 @@
 package pkg;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Main branch update.");
-    }
+	public static void main(String[] args) {
+		System.out.println("Main branch update.");
+	}
+
 }
