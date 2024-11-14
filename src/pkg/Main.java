0e17ff022d31d36b03bbdec9d00f5d7923e86103
MuhammadSarim07 <f223688@cfd.nu.edu.pk>
2024-11-14 11:26:05 +0500

    Introduced a bug

--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -3,7 +3,7 @@ package pkg;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the application!");
-		greetUser();
+		// greetUser(); // Removed this line to introduce a bug
 	}
 
 	public static void greetUser() {
